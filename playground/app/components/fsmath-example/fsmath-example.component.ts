fb376d15b69f2fcb05e678311926a48781b0c585
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-03-01 11:02:30 +0300

    FC-T62 FsMath deprecated from now

--- playground/app/components/fsmath-example/fsmath-example.component.ts
+++ playground/app/components/fsmath-example/fsmath-example.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
-import { FsMath } from './../../../../src/services/fsmath.service';
+import {Component} from '@angular/core';
+import {FsMath} from './../../../../src/services/fsmath.service';
+
+import {round} from '../../../../src/math';
 
 @Component({
   selector: 'fsmath-example',
   templateUrl: 'fsmath-example.component.html',
-  styleUrls: [ 'fsmath-example.component.css' ]
+  styleUrls: ['fsmath-example.component.css']
 })
 export class FsMathExampleComponent {
 
   examples = [];
+
   constructor(public FsMath: FsMath) {
-    this.examples = [ {  name: 'Round',
-                          code: "FsMath.round(10.53,1)",
-                          result: FsMath.round(10.53,1) }
-                      ];
+    this.examples = [
+      {
+        name: 'Round',
+        deprecatedCode: 'FsMath.round(10.53,1)',
+        code: `
+          import { round } from '@firestitch/common/math'; \n
+          round(10.53, 1)
+        `,
+        result: round(10.53, 1)
+      }
+    ];
 
     this.examples.forEach((example) => {
       example.result = JSON.stringify(example.result);
