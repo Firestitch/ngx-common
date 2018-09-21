3c7396c047a04dd6a773861d94b9ffbda6ab64f1
Anatoly <mendor1990@gmail.com>
2018-09-21 12:55:24 +0300

    MB-T32

--- playground/app/components/fsformat-pipes/fsformat-pipes.component.ts
+++ playground/app/components/fsformat-pipes/fsformat-pipes.component.ts
@@ -12,8 +12,8 @@ export class FsFormatPipesComponent {
     this.pipes.number = '{{42397803.23987|fsFormatNumber:2}}';
     this.pipes.currency = {
       simple: `{{42397803.23987|fsFormatCurrency}}`,
-      eur: `{{42397803.23987|fsFormatCurrency:0:'EUR'}}`,
-      precision: `{{42397803.23987|fsFormatCurrency:2:'EUR'}}`,
+      eur: `{{42397803.23987|fsFormatCurrency:2:'EUR'}}`,
+      precision: `{{42397803.23987|fsFormatCurrency:0:'EUR'}}`,
     };
   }
 
