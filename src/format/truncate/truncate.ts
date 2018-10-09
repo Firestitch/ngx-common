545ae2494505f68e49ae21dc7b298e070bd0e92e
Anatoly <mendor1990@gmail.com>
2018-10-09 13:58:25 +0300

    truncate

--- src/format/truncate/truncate.ts
+++ src/format/truncate/truncate.ts
@@ -1,12 +1,12 @@
 export function truncate(value: string, limit = 100, completeWords = false, ellipsis = '...') {
 
-  let result = value;
+  value = value || '';
 
   if (completeWords) {
     limit = value.substr(0, limit).lastIndexOf(' ');
   }
 
-  result = `${value.substr(0, limit)}`;
+  let result = `${value.substr(0, limit)}`;
 
   if (result !== value) {
     result += ellipsis;
