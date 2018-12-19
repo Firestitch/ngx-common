87a51c7f7f3885dc7fce37fdd34ac53a3e4c2916
Anatoly <mendor1990@gmail.com>
2018-12-19 17:18:36 +0200

    FC-T178

--- src/format/currency/currency.ts
+++ src/format/currency/currency.ts
@@ -2,5 +2,5 @@ import { CurrencyPipe } from '@angular/common';
 
 
 export function currency(amount, precision = 2, currencyCode = 'USD', locale = 'en-US') {
-  return new CurrencyPipe(locale).transform(amount, currencyCode, 'symbol', `1.0-${precision}`);
+  return new CurrencyPipe(locale).transform(amount, currencyCode, 'symbol', `1.${precision}-${precision}`);
 }
