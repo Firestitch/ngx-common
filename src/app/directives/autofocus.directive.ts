3a85d92ee343147b800bb87b7a29ed7de927409d
Raymond Gigliotti <ray@firestitch.com>
2023-12-12 18:14:34 -0500

    trying https://stackoverflow.com/questions/72433442/displaying-keyboard-in-safari-ios-after-focus-on-input-after-page-loads-modal-s

--- src/app/directives/autofocus.directive.ts
+++ src/app/directives/autofocus.directive.ts
@@ -23,11 +23,23 @@ export class FsAutofocusDirective implements AfterViewInit {
 
   public focus(): void {
     setTimeout(() => {
+      const tmpEl = document.createElement('input');
+      tmpEl.style.width = '0';
+      tmpEl.style.height = '0';
+      tmpEl.style.margin = '0';
+      tmpEl.style.padding = '0';
+      tmpEl.style.border = '0';
+      tmpEl.style.opacity = '0';
+      document.body.appendChild(tmpEl);
+      tmpEl.focus();
+
       if (this._matInput) {
         this._matInput.focus();
       } else if (this._el.nativeElement.focus) {
         this._el.nativeElement.focus();
       }
+
+      document.body.removeChild(tmpEl);
     }, 50);
   }
 }
